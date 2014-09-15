5dc46e956ca3973ecf48020c8de48fddeccb61a7
Atom <fromatom@gmail.com>
2014-09-15 17:06:31 +0900

    ON OFFを切り替えられるように（OFFが上手く動作しない）

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+this.enable = true;
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     chrome.tabs.executeScript(null, {
         file: "main.js"
@@ -10,16 +12,46 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
     if(true) {
         chrome.pageAction.show(tabId);
+        chrome.pageAction.setIcon({
+            tabId: tab.id,
+            path: 'on.png'
+        });
     }
 });
 
-chrome.pageAction.onClicked.addListener(function() {
-    chrome.tabs.executeScript(null, {
-        "code": 'hatenaBlogAutoReloader.disableAutoReloader();'
-    }, function(result) {
-        if (chrome.runtime.lastError) {
-            console.log('ERROR: ' + chrome.runtime.lastError.message);
-            return;
-        }
-    });
+chrome.pageAction.onClicked.addListener(function(tab) {
+    if (this.enable) {
+        chrome.tabs.executeScript(null, {
+            "code": 'hatenaBlogAutoReloader.disableAutoReloader();'
+        }, function(result) {
+            if (chrome.runtime.lastError) {
+                console.log('ERROR: ' + chrome.runtime.lastError.message);
+                return;
+            }
+        });
+
+        chrome.pageAction.setIcon({
+            tabId: tab.id,
+            path: 'off.png'
+        });
+
+        this.enable = false;
+    }
+    else {
+        chrome.tabs.executeScript(null, {
+            "code": 'hatenaBlogAutoReloader.enableAutoReloader();'
+        }, function(result) {
+            if (chrome.runtime.lastError) {
+                console.log('ERROR: ' + chrome.runtime.lastError.message);
+                return;
+            }
+        });
+
+        chrome.pageAction.setIcon({
+            tabId: tab.id,
+            path: 'on.png'
+        });
+
+        this.enable = true;
+    }
 });
