2791bff58faa58f22924031441dbf6adf1742182
Atom <fromatom@gmail.com>
2014-09-15 14:59:13 +0900

    問答無用で定期リロードが可能

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
 var HatenaBlogAutoReloader = function () {
+    this.state = false;
 };
 
-HatenaBlogAutoReloader.prototype.sendAlert = function () {
+HatenaBlogAutoReloader.prototype.isEnableExternalPreview = function () {
+    var blogicon_external_element = document.querySelector('#ui-id-2 i.blogicon-external');
+    var is_display = blogicon_external_element.style.display;
+
+    if (is_display === 'none') {
+        return true;
+    }
+    return false;
+};
+
+HatenaBlogAutoReloader.prototype.isUpdateEditarea = function () {
+};
+
+HatenaBlogAutoReloader.prototype.reload = function () {
+    var blogicon_repeat_element = document.querySelector('#ui-id-2 i.blogicon-repeat');
+    var click_event = document.createEvent("MouseEvents");
+    click_event.initEvent("click", false, true);
+    blogicon_repeat_element.dispatchEvent(click_event);
+};
+
+HatenaBlogAutoReloader.prototype.setAutoReloader = function () {
+    var blogicon_repeat_element = document.querySelector('#ui-id-2 i.blogicon-repeat');
+    var interval_time = 5000;
+    var self = this;
+
+    setInterval(function () {
+        self.reload();
+   }, interval_time);
+};
+
+HatenaBlogAutoReloader.prototype.removeAutoReloader = function () {
     console.log('HI!!!');
 };
 
-
-var hatenaBlogAutoReloader = new HatenaBlogAutoReloader();
-hatenaBlogAutoReloader.sendAlert();
+if (typeof hatenaBlogAutoReloader === 'undefined') {
+    var hatenaBlogAutoReloader = new HatenaBlogAutoReloader();
+    hatenaBlogAutoReloader.setAutoReloader();
+}
