006581ebf6fe387a20ce2251eb19831f7adcd219
Atom <fromatom@gmail.com>
2014-09-15 14:04:58 +0900

    整理

--- background.js
+++ background.js
@@ -1,32 +1,30 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.tabs.executeScript(null, {
+        file: "main.js"
+    }, function(result) {
+        if (chrome.runtime.lastError) {
+            console.log('ERROR: ' + chrome.runtime.lastError.message);
+            return;
+        }
+    });
+
     if(true) {
         chrome.pageAction.show(tabId);
     }
 });
 
-function setAutoReloader() {
-    var target_element = $('.blogicon-external js-icon-preview-in-new-window preview-in-new-window tipsy-top');
-    //var click_event = $().createEvent("MouseEvents");
-    //click_event.initEvent("click", false, true);
-    //target_element.dispatchEvent(click_event);
-    target_element.trigger("click");
-    console.log(target_element.attr('original-title'));
-}
-
 chrome.pageAction.onClicked.addListener(function() {
     chrome.tabs.executeScript(null, {
-        "code": setAutoReloader()
+        "code": 'hatenaBlogAutoReloader.sendAlert();'
     }, function(result) {
-        if (chrome.runtime.lastError) { // or if (!result)
+        if (chrome.runtime.lastError) {
             console.log('ERROR: ' + chrome.runtime.lastError.message);
-            // Get the error message via chrome.runtime.lastError.message
             return;
         }
     });
 });
 
-// chrome.browserAction.onClicked.addListener(function () {
-//     chrome.tabs.executeScript(null, {
-//         "code": setAutoReloader()
-//     });
-// });
+//     //var click_event = $().createEvent("MouseEvents");
+//     //click_event.initEvent("click", false, true);
+//     //target_element.dispatchEvent(click_event);
+//     //target_element.trigger("click");
---
+++ main.js
@@ -0,0 +1,10 @@
+var HatenaBlogAutoReloader = function () {
+};
+
+HatenaBlogAutoReloader.prototype.sendAlert = function () {
+    console.log('HI!!!');
+};
+
+
+var hatenaBlogAutoReloader = new HatenaBlogAutoReloader();
+hatenaBlogAutoReloader.sendAlert();
